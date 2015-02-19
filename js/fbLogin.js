c6872c69d57918c535ee042a9f6d714d3c09346f
Tim Mendez <hikeemhigh@gmail.com>
2015-02-18 22:34:50 -0800

    testing change

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -1,7 +1,5 @@
 // This is called with the results from from FB.getLoginStatus().
 function statusChangeCallback(response) {
- console.log('statusChangeCallback');
- console.log(response);
  // The response object is returned with a status field that lets the
  // app know the current login status of the person.
  // Full docs on the response object can be found in the documentation
@@ -52,18 +50,18 @@ FB.init({
 // These three cases are handled in the callback function.
 
 FB.getLoginStatus(function(response) {
- statusChangeCallback(response);
+   statusChangeCallback(response);
 });
 
 };
 
 // Load the SDK asynchronously
 (function(d, s, id) {
- var js, fjs = d.getElementsByTagName(s)[0];
- if (d.getElementById(id)) return;
- js = d.createElement(s); js.id = id;
- js.src = "//connect.facebook.net/en_US/sdk.js";
- fjs.parentNode.insertBefore(js, fjs);
+   var js, fjs = d.getElementsByTagName(s)[0];
+   if (d.getElementById(id)) return;
+   js = d.createElement(s); js.id = id;
+   js.src = "//connect.facebook.net/en_US/sdk.js";
+   fjs.parentNode.insertBefore(js, fjs);
 }(document, 'script', 'facebook-jssdk'));
 
 // Here we run a very simple test of the Graph API after login is
@@ -73,6 +71,6 @@ function testAPI() {
  FB.api('/me', function(response) {
    console.log('Successful login for: ' + response.name);
    document.getElementById('status').innerHTML =
-     'Thanks for logging in, ' + response.name + '!';
+     'Thanks for logging in, ' + response.id + '!';
  });
 }
