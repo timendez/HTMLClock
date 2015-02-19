eac8fdfb357f298687ecb307c5bf34a7cdb9444f
Tim Mendez <hikeemhigh@gmail.com>
2015-02-18 22:44:32 -0800

    testing change

--- js/fbLogin.js
+++ js/fbLogin.js
@@ -67,10 +67,9 @@ FB.getLoginStatus(function(response) {
 // Here we run a very simple test of the Graph API after login is
 // successful.  See statusChangeCallback() for when this call is made.
 function testAPI() {
- console.log('Welcome!  Fetching your information.... ');
- FB.api('/me', function(response) {
-   console.log('Successful login for: ' + response.name);
-   document.getElementById('status').innerHTML =
-     'Thanks for logging in, ' + response.id + '!';
- });
+  FB.api('/me', function(response) {
+     $("#loginButton").addClass("hide");
+    document.getElementById('loginButton').
+     document.getElementById('userName').innerHTML = response.name;
+  });
 }
